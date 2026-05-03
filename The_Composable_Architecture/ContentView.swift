42dfe9ecf3b253e1b00e80032ceb1b42d5820c05
Tejaskumar Patel <tejas786u@gmail.com>
2026-05-03 22:46:26 +0200

    Added SideEffects (API Integration and Timer)

--- The_Composable_Architecture/ContentView.swift
+++ The_Composable_Architecture/ContentView.swift
@@ -37,6 +37,28 @@ struct ContentView: View {
                     .padding()
                     .background(Color.black.opacity(0.1))
                     .cornerRadius(10)
+                    Button(viewStore.isTimerRunning ? "Stop Timer" : "Start Timer") {
+                        viewStore.send(.toggleTimerButtonTapped)
+                    }
+                    .font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                }
+                Button("Fact") {
+                    viewStore.send(.factButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                if viewStore.isLoading {
+                    ProgressView()
+                } else if let fact = viewStore.fact{
+                    Text(fact)
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .padding()
                 }
             }
         }
--- The_Composable_Architecture/CounterFeature.swift
+++ The_Composable_Architecture/CounterFeature.swift
@@ -11,13 +11,24 @@ import ComposableArchitecture
 // 1. Make it conform of Reducer
 struct CounterFeature: Reducer {
     // 2. Add stubs Struct State and enum Action
-    struct State {
+    struct State: Equatable {
         var count: Int = 0
+        var isLoading: Bool = false
+        var fact: String?
+        var isTimerRunning: Bool = false
     }
     
     enum Action {
         case incrementButtonTapped
         case decrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        case toggleTimerButtonTapped
+        case timerTick
+    }
+    
+    nonisolated enum CancelID {
+        case timer
     }
     
     // 3. Confirm reduce func and map action with changing states
@@ -25,13 +36,44 @@ struct CounterFeature: Reducer {
         switch action {
         case .incrementButtonTapped:
             state.count += 1
+            state.fact = nil
             return .none
         case .decrementButtonTapped:
             state.count -= 1
+            state.fact = nil
+            return .none
+        case .factButtonTapped:
+            state.fact = nil
+            state.isLoading = true
+            return .run { [count = state.count] send in
+                let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                let fact = String(decoding: data, as: UTF8.self)
+                await send(.factResponse(fact))
+            }
+        case .factResponse(let fact):
+            state.fact = fact
+            state.isLoading = false
+            return .none
+        case .toggleTimerButtonTapped:
+            state.isTimerRunning.toggle()
+            if state.isTimerRunning {
+                return .run { send in
+                    while true {
+                        try await Task.sleep(for: .seconds(1))
+                        await send(.timerTick)
+                    }
+                }
+                .cancellable(id: CancelID.timer)
+            } else {
+                return .cancel(id: CancelID.timer)
+            }
+        case .timerTick:
+            state.count += 1
+            state.fact = nil
             return .none
         }
     }
 }
 
 // 6. WithViewStore required equatable that's why added extesnion
-extension CounterFeature.State: Equatable {}
+//extension CounterFeature.State: Equatable {}
