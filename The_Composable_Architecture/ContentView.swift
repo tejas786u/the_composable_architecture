2cf3f7d232cac80488bffc11dc2b463e5cb13db0
Tejaskumar Patel <tejas786u@gmail.com>
2026-05-03 17:43:33 +0200

    Initial Commit

---
+++ The_Composable_Architecture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The_Composable_Architecture
+//
+//  Created by Tejas Patel on 03/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The_Composable_Architecture/The_Composable_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Composable_ArchitectureApp.swift
+//  The_Composable_Architecture
+//
+//  Created by Tejas Patel on 03/05/26.
+//
+
+import SwiftUI
+
+@main
+struct The_Composable_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
